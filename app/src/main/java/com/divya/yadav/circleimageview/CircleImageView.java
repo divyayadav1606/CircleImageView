bf289dd5230bc917ebd5aad93ab3e8c4598d25af
Divya Yadav <divyayadav1606@gmail.com>
2017-05-12 11:01:04 -0700

    Initial Check in
    1. Main Activity with an Image View

---
+++ app/src/main/java/com/divya/yadav/circleimageview/CircleImageView.java
@@ -0,0 +1,13 @@
+package com.divya.yadav.circleimageview;
+
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+public class CircleImageView extends android.support.v7.widget.AppCompatImageView {
+
+    public CircleImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
